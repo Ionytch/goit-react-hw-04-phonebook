2cf7c13ecb1a1089066a6646970b48b0a92b5eb9
Ionytch <starik.ionytch@gmail.com>
2023-03-06 19:13:36 +0200

    hooks added

--- src/components/phonebook/Phonebook.js
+++ src/components/phonebook/Phonebook.js
@@ -1,35 +1,50 @@
- import { Component } from "react";
+import { useState } from "react";
 
-class Phonebook extends Component {
+export default function Phonebook() {
+    // state = {
+    //     name: '',
+    //     number: '',
+    // };
+
+    // handleChange = e => {
+    //     const { name, value } = e.currentTarget;
+    //     this.setState({ [name]: value });
+    // };
+
+    // handleSubmit = e => {
+    //     e.preventDefault();
+    //     const { onSubmit } = this.props;
+    //     onSubmit(this.state);
+    //     this.reset();
+    // };
+
+    // reset = () => {
+    //     this.setState({ name: '', number: '' });
+    // };
+
+
+const [name, setName]=useState('');
+const [number, setNumber]=useState('');
     
-    state = {
-        name: '',
-        number: '',
-    };
-
-    handleChange = e => {
-        const { name, value } = e.currentTarget;
-        this.setState({ [name]: value });
-    };
-
-    handleSubmit = e => {
-        e.preventDefault();
-        const { onSubmit } = this.props;
-        onSubmit(this.state);
-        this.reset();
-    };
-
-    reset = () => {
-        this.setState({ name: '', number: '' });
-    };
-
-
-
-    render() {
+    const handleChange=event=>{
+        const{name, value}=event.target;
+       console.log (event.target.name);
+       switch (name) {
+        case 'name':
+            setName(value);
+            break;
+            case 'number':
+                setNumber(value);
+                break;
+        default:
+            break;
+       }
+    }
+    
 
         return (<div>
             <h2>PHONEBOOK</h2>
-            <form onSubmit={this.handleSubmit}>
+            <form >
                 <label for="name">
                     NAME
                 </label>
@@ -38,8 +53,8 @@ class Phonebook extends Component {
                     name="name"
                     pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                     title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                    value={this.state.name}
-                    onChange={this.handleChange}
+                    value={name}
+                    onChange={handleChange}
                     required
                 ></input>
                 <label for="number">
@@ -50,8 +65,8 @@ class Phonebook extends Component {
                     name="number"
                     pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                     title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                    value={this.state.number}
-                    onChange={this.handleChange}
+                    value={number}
+                    onChange={handleChange}
                     required
                 ></input>
                 <button type="submit">ADD CONTACT</button>
@@ -60,7 +75,9 @@ class Phonebook extends Component {
         </div>
        
         )
-    }
+    
 }
 
-export default Phonebook
+// export default Phonebook
+
+// onSubmit={this.handleSubmit}
